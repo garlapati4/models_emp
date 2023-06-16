784c99174ad7f4722a4d251e78941f4cad8847fb
garlapati4 <garlapatinikithachowdary@gmail.com>
2023-06-16 17:43:02 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Emp(models.Model):
+    name=models.CharField(max_length=100)
+    id=models.IntegerField(primary_key=True)
+    sal=models.IntegerField()
+class Dept(models.Model):
+    name=models.ForeignKey(Emp,on_delete=models.CASCADE)
+    loc=models.CharField(max_length=100)
